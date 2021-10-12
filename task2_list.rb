66855a72b9f11e033dd2d87e70026b439c57d594
IronArt <poluvesov.artyom@gmail.com>
2021-10-12 23:27:25 +0400

    исправил имена

--- task2_list.rb
+++ task2_list.rb
@@ -1,10 +1,10 @@
 require 'benchmark'
 
-class Elem
-    attr_accessor :val, :nxt
-    def initialize(val=0, nxt=0)
-        @val = val
-        @nxt = nxt
+class Element
+    attr_accessor :value, :next
+    def initialize(value=0, next=0)
+        value = value
+        next = next
     end
 end
 
@@ -12,68 +12,68 @@ class List
     attr_writer :start
     
     def initialize
-        @start = Elem.new()
+        @start = Element.new()
     end
     
     def count
-        @cnt = 0
-        @now = @start
-        while @now.nxt != 0
-            @now = @now.nxt
-            @cnt += 1
+        count = 0
+        now = @start
+        while now.next != 0
+            now = now.next
+            count += 1
         end
-        return @cnt
+        return count
     end
     
-    def prepend(val)
-        new_el = Elem.new(val, @start.nxt)
-        @start.nxt = new_el
+    def prepend(value)
+        new_element = Element.new(value, @start.next)
+        @start.next = new_element
     end
     
-    def append(val)
-        new_el = Elem.new(val)
-        @now = @start
-        while @now.nxt != 0
-            @now = @now.nxt
+    def append(value)
+        new_element = Element.new(value)
+        now = @start
+        while now.next != 0
+            now = now.next
         end
-        @now.nxt = new_el
+        now.next = new_element
     end
     
-    def insert(val, ind)
-        @cnt = 0
-        @now = @start
-        while @now.nxt != 0 and @cnt != ind
-            @now = @now.nxt
-            @cnt += 1
+    def insert(value, index)
+        count = 0
+        now = @start
+        while now.next != 0 and count != index
+            now = now.next
+            count += 1
         end
-        if @cnt == ind
-            @now.nxt = Elem.new(val, @now.nxt)
+        if count == index
+            now.next = Element.new(value, now.next)
         end
     end
     
-    def get(ind)
-        @cnt = -1
-        @now = @start
-        while @now.nxt != 0 and @cnt != ind
-            @now = @now.nxt
-            @cnt += 1
+    def get(index)
+        count = -1
+        now = @start
+        while now.next != 0 and count != index
+            now = now.next
+            count += 1
         end
-        if @cnt == ind
-            return @now.val
+        if count == index
+            return now.value
         end
         return "not found"
     end
     
-    def remove(ind)
-        @cnt = -1
-        @second = @start
-        while @second.nxt != 0 and @cnt != ind
-            @first = @second
-            @second = @second.nxt
-            @cnt += 1
+    def remove(index)
+        count = -1
+        second = @start
+        while second.next != 0 and count != index
+            first = second
+            second = second.next
+            count += 1
         end
-        if @cnt == ind
-            @first.nxt = @second.nxt
+        if count == index
+            first.next = second.next
         end
         return "not found"
     end
